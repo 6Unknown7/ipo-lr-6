01cb9e435b0930c0ec37480bdfbb0a42d0acd2ad
Unknown <korolevdaniilwapara@gmail.com>
2025-11-26 11:01:32 +0300

    GG

--- task3.py
+++ task3.py
@@ -1,10 +1,13 @@
+#Даниил
+
 count = int(input('Введите количество строки: '))
 if (count <= 0):
     print('Ошибка')
 else:
     list = []
+    string_count = 0
     for i in range(count):
-        string = input('\nВведите строки:')
+        string = input('\nВведите строки: ')
         list.append(string)
-    print(list)
-#доделать 
+        string_count+=1
+print ('Строк:',string_count, 'шт.')
